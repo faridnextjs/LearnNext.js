a0984befb197e4ae75618c5ea41eb3d3aa4236f0
Illusion <17003685+faridnextjs@users.noreply.github.com>
2024-10-14 12:01:00 +0200

    added dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  display: "swap",
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+});
